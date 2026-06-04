fcffc831ee05d4014a830104d1970f9db4d2b9f6
Alejandro Torres González <alextggithub@gmail.com>
2026-06-04 18:15:14 +0200

    Actualización Service  Worker

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,46 @@
-const CACHE_NAME = "gfitapp-v2";
-
-const urlsToCache = [
-    "./",
-    "./index.html",
-    "./perfil.html",
-    "./reservas.html",
-    "./historial.html",
-    "./clases.html",
-    "./styles.css",
-    "./script.js",
-    "./manifest.json",
-    "./image_Gfit_180.png",
-    "./image_Gfit_192.png",
-    "./image_Gfit_512.png"
-];
-
+const CACHE_NAME = "gfitapp-v3";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./perfil.html",
+        "./reservas.html",
+        "./historial.html",
+        "./clases.html",
+        "./manifest.json",
+        "./image_Gfit_180.png",
+        "./image_Gfit_192.png",
+        "./image_Gfit_512.png"
+      ]);
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys.map(key => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            )
-        )
-    );
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
